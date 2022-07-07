0ec2cd3cbdcf7f227dec45cffd76d1c4c8ad1e45
Your Name <you@example.com>
2022-07-07 01:38:01 -0700

    isupper task

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: an integer parameter
+ * Return: success
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
