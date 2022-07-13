7fb614c2d45d03bfabc0cda4f52817a07498c841
Your Name <you@example.com>
2022-07-12 18:09:26 -0700

    correction

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
  * reset_to_98 - takes a pointer to int as parameter and update the value it points to 98
  * @n: pointer to n to be updated
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
