4e3674438af3d5435b56d01c8550bc95a2e49865
Your Name <you@example.com>
2022-07-12 18:41:35 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string pointer to be passed
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
